db02e16f2e8cc182482a79173d8228b8b61da94f
Maciej Hałas <maciejhalas@outlook.com>
2023-04-13 20:49:42 +0200

    Remove simba adventure game

--- simba/js/objects/exitSign.js
+++
@@ -1,9 +0,0 @@
-var exitSigns;
-
-function InitializeExitSign(x,y) {
-    exitSigns = game.add.group();
-    exitSigns.enableBody = true;
-
-    var es = exitSigns.create(x,y, 'platform', 'exitSign');
-    es.body.immovable = true;
-}
